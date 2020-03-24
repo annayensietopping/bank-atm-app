75c740fb346ec20f33b3e3eadab6acee9477d476
oona baby <oonababy@noahs-mbp-2.lan>
2020-03-23 21:06:59 -0400

    created classes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,63 @@
 $(function () {
+
+// create a new class
+class BankAccount {
+  constructor(accountType, amount) {
+    this._accountType = accountType
+    this._amount = 0
+    this._transactionHistory = []
+  }
+
+
+// add the methods
+  withdrawl(withdrawlAmount) {
+    // store balance before change
+    const oldBalance = this._amount
+
+    this._amount = this._amount - withdrawlAmount
+
+    this._transactionHistory.push({
+      oldBalance: oldBalance,
+      transactionType: 'withdrawl' ,
+      transactionAmount: withdrawlAmount,
+      newBalance: this._amount,
+    })
+  }
+
+  deposit(depositAmount) {
+const oldBalance = this._amount
+
+    this._amount = this._amount + depositAmount
+
+    this._transactionHistory.push({
+      oldBalance: oldBalance,
+      transactionType: 'deposit' ,
+      transactionAmount: depositAmount,
+      newBalance: this._amount,
+    })
+  }
+
+  showBalance() {
+    return this._amount
+  }
+
+  get transactionHistory() {
+    return this._transactionHistory
+  }
+// end methods
+}
+// end new class
+
+const checkingAccount = new BankAccount('checking')
+
+// testing
+console.log(checkingAccount)
+checkingAccount.deposit(100)
+console.log(checkingAccount)
+checkingAccount.withdrawl(75)
+checkingAccount.withdrawl(50)
+console.log(checkingAccount)
+
+console.log(checkingAccount.showBalance())
+console.log(checkingAccount)
 })
