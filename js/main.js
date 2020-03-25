15789e588770ac934be08f6384b008673ee05ea6
oona baby <oonababy@noahs-mbp-2.lan>
2020-03-25 19:29:49 -0400

    completed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(function () {
 
+
 $('#transaction-form').submit((event) => {
   event.preventDefault()
 
@@ -10,6 +11,7 @@ console.log(amount)
 console.log(transactionType)
 
 processTransaction(transactionType, amount)
+clearField()
 })
 
 
@@ -19,9 +21,46 @@ function processTransaction(transactionType, amount) {
   } else {
     checkingAccount.withdrawl(amount)
   }
-
+updateUi()
+displayTransactions()
   console.log(checkingAccount.transactionHistory)
 }
+
+function displayTransactions() {
+  const t = checkingAccount.transactionHistory
+  const transactionsHtml = t.map((transaction) => {
+    return buildTransactionHtml(
+      transaction.transactionType,
+      transaction.transactionAmount,
+      transaction.newBalance
+    )
+  })
+
+  console.log(transactionsHtml)
+  $('#transaction-list').html(transactionsHtml)
+}
+
+function updateUi() {
+  // display current bank bank-balance
+  // call class and then call function that gets current balance
+  $('.bank-balance').text(checkingAccount.showBalance())
+}
+
+function clearField() {
+  $('#transaction-amount').val(" ")
+}
+
+function buildTransactionHtml (txType, txAmount, newBalance) {
+   const transactionHtml = (`
+     <li>
+       <span class='new-transaction-type'>${txType}</span>
+       <span>for</span>
+       <span class='new-transaction-amount'>$${txAmount}<span> |
+       <span>New Balance:</span> <span class='new-bank-balance'>$${newBalance}<span>
+     </li>
+   `)
+   return transactionHtml
+ }
 
 // create a new class
 class BankAccount {
@@ -72,6 +111,7 @@ const oldBalance = this._amount
 // end new class
 
 const checkingAccount = new BankAccount('checking')
+updateUi()
 
 // testing
 // console.log(checkingAccount)
